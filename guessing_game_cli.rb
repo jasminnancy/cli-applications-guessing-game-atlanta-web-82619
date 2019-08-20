6a260e9ff7767a6193aaf4bde1498e0238f77254
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 23:49:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,4 +3,10 @@ def run_guessing_game
   random = rand(1..6)
   prompt user
   gets.chomp
+  
+  if gets.chomp == random
+    puts "you guessed the correct number!"
+  elsif gets.chomp != random
+    puts "Sorry! The computer guessed #{random}"
+  elsif gets.chomp == "exit"
 end
