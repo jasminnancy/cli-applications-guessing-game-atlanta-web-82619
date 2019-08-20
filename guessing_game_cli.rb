18e51a6d5f71987a39900db7ea84d2506f8ea73f
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 23:57:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,6 @@ def run_guessing_game
   random = rand(1..6)
   user_input = gets.chomp
   
-  random
-  prompt user
-  user_input
-  
   if user_input.to_i == random
     puts "you guessed the correct number!"
   elsif user_input.downcase == "exit"
